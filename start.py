57dcdf3600352626866b5ba13e432ab936db86a3
Falshon <xhgdpropro@gmail.com>
2024-09-22 16:32:39 +0200

    programs

---
+++ start.py
@@ -0,0 +1,80 @@
+﻿import os
+import time
+from colorama import Fore, Back
+import colorama
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+import time
+
+print ("")
+time.sleep(0.1)
+print("")
+time.sleep(0.1)
+print("                  .____ _________   _____________________    _____  _________  ____  __.______________________ ")
+time.sleep(0.1)
+print("                  |   \ ______   \  \__    ___/\______   \  /  _  \ \_   ___ \|    |/ _|\_   _____/\______   |  ")
+time.sleep(0.1)
+print("                  |   ||     ___/     |    |    |       _/ /  /_\  \/    \  \/|      <   |    __)_  |       _/")
+time.sleep(0.1)
+print("                  |   ||    |         |    |    |    |   \/    |    \     \___|    |  \  |        \ |    |   | ")
+time.sleep(0.1)
+print("                  |___||____|         |____|    |____|_  /\____|__  /\______  /____|__ \/_______  / |____|_  /")
+time.sleep(0.1)
+print("                                                       \/         \/        \/        \/        \/         \/ ")
+time.sleep(0.1)
+print("")
+time.sleep(0.1)
+print("")
+time.sleep(0.1)
+time.sleep(0.1)
+print("")
+print("                                                > Discord : Falshon.py")
+time.sleep(0.1)
+print("")
+print("                                                > Developper : Falshon")
+time.sleep(0.1)
+print("")
+print("                                                > Version : 1.0")
+time.sleep(0.1)
+print("")
+print("")
+time.sleep(0.1)
+print("               [ 01 ] Track IP")
+time.sleep(0.1)
+print("               [ 02 ] Ping IP (soon..)")
+time.sleep(0.1)
+print("               [ 03 ] IP Port Scanner (soon..)")
+time.sleep(0.1)
+print("               [ 04 ] IP Scanner (sonn..)")
+time.sleep(0.1)
+print("               [ 05 ] About us.")
+time.sleep(0.1)
+print("               [ 06 ] Help / How to use")
+print("")
+print("")
+print("")
+
+time.sleep(0.1)
+print("  ┌──(user002@ip_tracker) - (~/Start)")
+print("  │")
+time.sleep(0.1)
+choice = input("  └─$")
+
+if choice == '5':
+    os.system("python prgrm/aboutus.py")
+if choice == '05':
+    os.system("python prgrm/aboutus.py")
+if choice == '1':
+    os.system("python prgrm/trackip2.py")
+if choice == '01':
+    os.system("python prgrm/trackip2.py")
+if choice == '2' :
+    os.system("python prgrm/pingip.py")
+if choice == '02' :
+    os.system("python prgrm/pingip.py")
+if choice == '6' :
+    os.system("python prgrm/help.py")
+if choice == '06' :
+    os.system("python prgrm/help.py")
